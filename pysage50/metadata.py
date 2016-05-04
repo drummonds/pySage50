3e359c97a7a7a9af59fa89430c513998e2819519
Humphrey <hum3@drummond.info>
2016-05-04 10:25:18 +0100

    Updating release number

---
+++ pysage50/metadata.py
@@ -0,0 +1,17 @@
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'pysage50'
+project = "Python Sage 50 tools"
+project_no_spaces = project.replace(' ', '')
+version = '0.0.1'
+description = 'A pythonic interface to Sage Line 50.'
+authors = ['drummonds']
+authors_string = ', '.join(authors)
+emails = ['hum3@drummond.info']
+license = 'MIT'
+copyright = '2016 ' + authors_string
+url = 'https://github.com/drummonds/pySage50'
