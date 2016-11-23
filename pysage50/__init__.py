5ac89e345d166776660fc6185df422d949c2f253
drummonds <humphrey.drummond@gmail.com>
2016-11-23 15:04:22 +0000

    Adding version

--- pysage50/__init__.py
+++ pysage50/__init__.py
@@ -2,3 +2,4 @@ from .sage_import import SageImport
 from .sage import Sage, PySageError
 from .sage_transaction import SageTransactionError, SageTransaction, SagePurchaseInvoice
 from .sage_transaction import SageTransactionPurchaseInvoice, SageTransactionPurchaseAdvance
+from .metadata import version
